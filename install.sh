4cf5434e81531c6f3b791bdcc5b5a30698c6bae8
Lewis <95539466+smartsyncing@users.noreply.github.com>
2022-01-02 03:03:46 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.config/.zsh &&
-rm -rf ~/.zshrc &&
 git clone https://github.com/smartsyncing/dotfiles/ &&
 cd dotfiles &&
 mv .zsh ~/.config &&
 mv .zshrc ~/ 
+
+git clone git://github.com/zsh-users/zsh-completions.git &&
+mv zsh-completions ~/config/.zsh/ &&
+echo "fpath=(~/.config/.zsh/zsh-completions/src $fpath)" >> ~/.zshrc &&
+rm -rf ~/.config/.zsh &&
+rm -rf ~/.zshrc &&
+
