7dfa73afd11373ad9c21f07e375568ae35b2701e
Lewis <95539466+smartsyncing@users.noreply.github.com>
2022-01-02 02:47:25 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#rm -rf ~/.config/.zsh &&
-#rm -rf ~/.zshrc &&
+rm -rf ~/.config/.zsh &&
+rm -rf ~/.zshrc &&
 git clone https://github.com/smartsyncing/dotfiles/ &&
 cd dotfiles &&
 mv .zsh ~/.config &&
