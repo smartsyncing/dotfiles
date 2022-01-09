c65eb50a83e26f9ee43119ddce7e675058e51553
smartsyncing <yes.vrocht@slmail.me>
2022-01-09 20:03:01 +0000

    Install script.

--- install.sh
+++ install.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 zshrc_file=$HOME/.zshrc
 zsh_dir=$HOME/.config/zsh
-
+neofetch_dir=$HOME/.config/neofetch
+kitty_dir=$HOME/.config/kitty
+vis_dir=$HOME/.config/vis
 
 if [ -d "$zsh_dir" ]; then
-  printf "\n\033[33mExisting ZSH config detected! Replacing...\033[0m...\n \n"
-  sudo rm -rf ~/.config/zsh && sudo rm -rf ~/.zshrc 
-elif command -v 'uninstall_oh_my_zsh' >'/dev/null' 2>&1; then
-  printf "\n\033[33mExisting ZSH config detected! Replacing...\033[0m...\n \n" 
-  uninstall_oh_my_zsh
-elif [ -d "$zshrc_file" ]; then
-  printf "\n\033[33mExisting ZSH config detected! Replacing...\033[0m...\n \n" 
-  sudo rm -rf ~/.zshrc
+  printf "\n\033[33mExisting ZSH config detected! Replacing...\033[0m\n "
+  rm -rf ~/.config/zsh && rm -rf ~/.zshrc 
 fi
-
+if command -v 'uninstall_oh_my_zsh' >'/dev/null' 2>&1; then
+  printf "\033[33mExisting ZSH config detected! Replacing...\033[0m\n" 
+  uninstall_oh_my_zsh
+fi
+if [ -d "$zshrc_file" ]; then
+  printf "\033[33mExisting ZSH config detected! Replacing...\033[0m\n" 
+  rm -rf ~/.zshrc
+fi
+if [ -d "$kitty_dir" ]; then
+  printf "\033[33mExisting Kitty config detected! Replacing...\033[0m\n"
+  rm -rf ~/.config/kitty
+fi
+if [ -d "$vis_dir" ]; then
+  printf "\033[33mExisting Vis config detected! Replacing...\033[0m\n"
+  rm -rf ~/.config/vis
+fi
+if [ -d "$neofetch_dir" ]; then
+  printf "\033[33mExisting Neofetch config detected! Replacing...\033[0m\n"
+  rm -rf ~/.config/neofetch
+fi
 
 printf "\n\033[33mInstalling needed dependencies\033[0m...\n \n"
 
 if command -v 'yay' >'/dev/null' 2>&1; then
-  yay -S --needed vim bat ttf-fira-code zsh kitty cli-visualizer lsd neofetch fzf zsh-completions-git
+  yay -S --needed vim bat ttf-fira-code zsh kitty cli-visualizer lsd neofetch fzf zsh-completions
 elif command -v 'paru' >'/dev/null' 2>&1; then
-  paru -S --needed vim bat ttf-fira-code zsh kitty cli-visualizer lsd neofetch fzf zsh-completions-git
+  paru -S --needed vim bat ttf-fira-code zsh kitty cli-visualizer lsd neofetch fzf zsh-completions
 elif command -v 'dnf' >'/dev/null' 2>&1; then
   sudo dnf install vim bat zsh fira-code-fonts kitty lsd neofetch fzf
   git clone https://github.com/dpayne/cli-visualizer && cd cli-visualizer
@@ -48,9 +63,10 @@ elif command -v 'zypper' >'/dev/null' 2>&1; then
   zypper install zsh-completions
 fi
 
-printf "\n\033[33mCloning the repo...\033[0m...\n \n"
+chsh -s /usr/bin/zsh
+printf "\n\033[33mCloning the repo...\033[0m\n \n"
 git clone https://github.com/smartsyncing/dotfiles && cd dotfiles
-printf "\n\033[33mMoving files...\033[0m...\n \n"
+printf "\n\033[33mMoving files...\033[0m\n \n"
 mv -f zsh ~/.config/zsh
 mv -f .zshrc ~/
 mv -f vis ~/.config
@@ -58,5 +74,4 @@ mv -f neofetch ~/.config
 mv -f kitty ~/.config
 cd .. 
 rm -rf dotfiles/
-
-printf "Finished installing."
+echo "Finished installing."
